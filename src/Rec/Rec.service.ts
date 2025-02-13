ced2dc2f6ba0235ce958114510799d0b57fc8ea9
Sergiu <sergiu.lungu@opentech.email>
2025-02-13 10:36:38 +0200

    add

--- src/Rec/Rec.service.ts
+++ src/Rec/Rec.service.ts
@@ -1,13 +1,14 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { getData } from 'src/supa-api.service';
 
 @Injectable()
 export class RecursiveService implements OnModuleInit {
   // Funcția recursivă
-  private callRecursiveFunction(): void {
+  private async callRecursiveFunction(): Promise<void> {
     console.log('Funcție recursivă apelată la fiecare secundă!');
-    
-    // Logica ta aici...
-    setTimeout(() => this.callRecursiveFunction(), 60000);
+    const models = await getData();
+    console.log(models[0])
+    setTimeout(() => this.callRecursiveFunction(), 10000);
   }
 
   // Pornire automată la inițializarea modulului
