8174492af87125f2a8a2e7a9c5dd6074a783c3f4
Sergiu <sergiu.lungu@opentech.email>
2025-02-13 03:57:03 +0200

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,6 @@ import { CheckModule } from './Check/check.module';
 import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-  imports:[CheckModule],
+  imports:[CheckModule, ScheduleModule.forRoot()],
 })
 export class AppModule {}
